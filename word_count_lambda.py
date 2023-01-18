6b096dd03af590326090610e2b9d079319464dd9
DebugWorld <23011873+DebugWorld-1024@users.noreply.github.com>
2023-01-18 15:56:50 +0800

    add socket source

--- word_count_lambda.py
+++ word_count_lambda.py
@@ -1,10 +1,14 @@
-from pyflink.datastream import StreamExecutionEnvironment
+from pyflink.datastream import StreamExecutionEnvironment, DataStream
 
 
 if __name__ == '__main__':
     env = StreamExecutionEnvironment.get_execution_environment()
     env.set_parallelism(2)
     # print(env.get_parallelism())
+
+    # Socket数据源
+    # socket_stream = DataStream(env._j_stream_execution_environment.
+    #                            socketTextStream('127.0.0.1', 9527))
 
     env.read_text_file("data/input/word_count.txt") \
         .flat_map(lambda x: str(x).split(",")) \
